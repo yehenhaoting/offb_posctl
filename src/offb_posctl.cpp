8afe4846c046f033fa4d0e23e7502314f79518e1
zm <yehenhaoting@sjtu.edu.cn>
2019-01-03 11:15:11 +0800

    add write log

--- src/offb_posctl.cpp
+++ src/offb_posctl.cpp
@@ -19,6 +19,7 @@
 #include <cstdlib>
 #include <stdlib.h>
 #include <iostream>
+#include <sstream>
 #include <stdio.h>
 #include <Eigen/Eigen>
 #include <Eigen/Geometry> 
@@ -51,22 +52,28 @@ mavros_msgs::State current_state;           //无人机当前状态
 geometry_msgs::PoseStamped pos_drone;       //读入的无人机当前位置
 geometry_msgs::TwistStamped vel_drone;      //读入的无人机当前速度
 geometry_msgs::PoseStamped att_drone;       //读入的无人机姿态
+geometry_msgs::Vector3 angle_receive;       //读入的无人机姿态（欧拉角）
 
 geometry_msgs::Quaternion orientation_target;   //发给无人机的姿态指令
+geometry_msgs::Vector3 angle_target;
+geometry_msgs::Vector3 vel_target;
 
 float thrust_target;        //期望推力
-
-float Euler[3];
-float Yaw_Locked;           //锁定的偏航角(一般锁定为0)
+float Yaw_Init;
+float Yaw_Locked = 0;           //锁定的偏航角(一般锁定为0)
 PID PIDVX, PIDVY, PIDVZ;    //声明PID类
 Parameter param;
+std::ofstream logfile;
+
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>声 明 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 //欧拉角转四元数
 geometry_msgs::Quaternion euler2quaternion(float roll, float pitch, float yaw);
-void quaternion2euler(float x, float y, float z, float w);
+geometry_msgs::Vector3 quaternion2euler(float x, float y, float z, float w);
+
 float get_ros_time(ros::Time time_begin);                                            //获取ros当前时间
 int pix_controller(float cur_time);                                                  //控制程序
+void data_log(float cur_time);
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>回 调 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
@@ -87,6 +94,7 @@ bool hasGotAtt = false;
 void att_cb(const geometry_msgs::PoseStamped::ConstPtr &msg){
     att_drone = *msg;
     hasGotAtt = true;
+    angle_receive = quaternion2euler(att_drone.pose.orientation.x, att_drone.pose.orientation.y, att_drone.pose.orientation.z, att_drone.pose.orientation.w);
 }
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>主 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -94,6 +102,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "position_control");
     ros::NodeHandle nh;
+    std::ofstream logfile;
 
     // 【订阅】无人机当前状态/位置/速度信息
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 20, state_cb);
@@ -107,12 +116,19 @@ int main(int argc, char **argv)
     ros::Rate rate(20.0);
 
     // 读取PID参数
-    
     std::string paraadr("/home/zm/catkin_ws/src/offb_posctl/src/param");
     if (param.readParam(paraadr.c_str()) == 0){
         std::cout<<"read config file error!"<<std::endl;
         return 0;
     }
+
+    // log输出文件初始化
+    logfile.open("/home/zm/catkin_ws/src/offb_posctl/src/log.csv", std::ios::out);
+    if (! logfile.is_open()){
+        std::cout<<"log to file error!"<<std::endl;
+        return 0;
+    }
+
 
     // 设置速度环PID参数 比例参数 积分参数 微分参数
     PIDVX.setPID(param.vx_p, param.vx_i, param.vx_d);
@@ -122,8 +138,6 @@ int main(int argc, char **argv)
     PIDVX.set_sat(2, 3, 0);
     PIDVY.set_sat(2, 3, 0);
     PIDVZ.set_sat(2, 5, 0);
-
-    
 
     // 等待和飞控的连接
     while(ros::ok() && current_state.connected == 0)
@@ -140,15 +154,13 @@ int main(int argc, char **argv)
         ros::spinOnce();
         ROS_INFO_STREAM("waitting for att...");
     }
-
-    ROS_INFO("Got Yaw_locked");
-
     float x = att_drone.pose.orientation.x;
     float y = att_drone.pose.orientation.y;
     float z = att_drone.pose.orientation.z;
     float w = att_drone.pose.orientation.w;
-    quaternion2euler(x, y, z, w);
-    ROS_INFO_STREAM("Got Yaw_locked: " << Yaw_Locked);
+    angle_receive = quaternion2euler(x, y, z, w);
+    Yaw_Init = angle_receive.z;
+    ROS_INFO_STREAM("Got Yaw_Init: " << Yaw_Init);
 
     // 记录启控时间
     ros::Time begin_time = ros::Time::now();
@@ -161,6 +173,19 @@ int main(int argc, char **argv)
         float cur_time = get_ros_time(begin_time);  // 当前时间
         pix_controller(cur_time);                   //控制程序
 
+        if(current_state.mode == "OFFBOARD"){
+            data_log(cur_time);                     //log输出
+        }
+        logfile <<cur_time<<","<<param.pos_x <<","<<param.pos_y <<","<<param.pos_z <<","                           //set_pos
+                <<pos_drone.pose.position.x <<","<<pos_drone.pose.position.y <<","<<pos_drone.pose.position.z <<","    //uav_pos
+                <<vel_target.x <<","<<vel_target.y <<","<<vel_target.z <<","                                           //set_vel
+                <<vel_drone.twist.linear.x <<","<<vel_drone.twist.linear.y <<","<<vel_drone.twist.linear.z <<","       //uav_vel
+                <<angle_target.x  <<","<<angle_target.y  <<","<<angle_target.z  <<","                                  //set_att
+                <<angle_receive.x <<","<<angle_receive.y <<","<<angle_receive.z <<","                                  //uav_att
+                <<thrust_target<<std::endl;
+
+
+
         std_msgs::Float32 data2pub;
         data2pub.data = thrust_target;
         thrust_pub.publish(data2pub);
@@ -168,7 +193,7 @@ int main(int argc, char **argv)
 
         rate.sleep();
     }
-
+    logfile.close();
     return 0;
 }
 
@@ -196,6 +221,9 @@ int pix_controller(float cur_time)
     float vel_xd = param.x_p * error_x;
     float vel_yd = param.y_p * error_y;
     float vel_zd = param.z_p * error_z;
+    vel_target.x = vel_xd;
+    vel_target.y = vel_yd;
+    vel_target.z = vel_zd;
 
 //速 度 环
     //积分标志位.未进入OFFBOARD时,不累积积分项;进入OFFBOARD时,开始积分.
@@ -225,16 +253,24 @@ int pix_controller(float cur_time)
             -cos(Yaw_Locked), sin(Yaw_Locked);
     Vector2f mat_temp(PIDVX.Output,PIDVY.Output);       //赋值到期望推力和姿态
     Vector2f euler_temp= 1/9.8 * A_yaw.inverse() * mat_temp;
-    Euler[0] = euler_temp[0];
-    Euler[1] = euler_temp[1];
-    Euler[2] = Yaw_Locked;
+    angle_target.x = euler_temp[0];
+    angle_target.y = euler_temp[1];
+//    angle_target.z = Yaw_Locked + Yaw_Init;
+    angle_target.z = Yaw_Locked;
 
-    orientation_target = euler2quaternion(Euler[0],Euler[1],Euler[2]);
+    orientation_target = euler2quaternion(angle_target.x, angle_target.y, angle_target.z);
     thrust_target = (float)(0.05 * (9.8 + PIDVZ.Output));   //目标推力值
 
     return 0;
 }
 
+/**
+ * 将欧拉角转化为四元数
+ * @param roll
+ * @param pitch
+ * @param yaw
+ * @return 返回四元数
+ */
 geometry_msgs::Quaternion euler2quaternion(float roll, float pitch, float yaw)
 {
     geometry_msgs::Quaternion temp;
@@ -245,11 +281,35 @@ geometry_msgs::Quaternion euler2quaternion(float roll, float pitch, float yaw)
     return temp;
 }
 
-void quaternion2euler(float x, float y, float z, float w)
+/**
+ * 将四元数转化为欧拉角形式
+ * @param x
+ * @param y
+ * @param z
+ * @param w
+ * @return 返回Vector3的欧拉角
+ */
+geometry_msgs::Vector3 quaternion2euler(float x, float y, float z, float w)
 {
-//    angle[0] = atan2(2.0 * (quat[3] * quat[2] + quat[0] * quat[1]), 1.0 - 2.0 * (quat[1] * quat[1] + quat[2] * quat[2]));
-//    angle[1] = asin(2.0 * (quat[2] * quat[0] - quat[3] * quat[1]));
-//    angle[2] = atan2(2.0 * (quat[3] * quat[0] + quat[1] * quat[2]), -1.0 + 2.0 * (quat[0] * quat[0] + quat[1] * quat[1]));
-//    Yaw_Locked = atan2(2.0 * (w * x + y * z), -1.0 + 2.0 * (x * x + y * y));
-    Yaw_Locked = (float)(atan2(2.0 * (w * z + x * y), +1.0 - 2.0 * (z * z + y * y)));
+    geometry_msgs::Vector3 temp;
+    temp.x = atan2(2.0 * (w * x + y * z), 1.0 - 2.0 * (x * x + y * y));
+    temp.y = asin(2.0 * (z * x - w * y));
+    temp.z = atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z));
+    return temp;
+}
+
+/**
+ * 将进入offboard后的位置&速度&姿态信息记录进文件
+ * @param cur_time
+ */
+void data_log(float cur_time)
+{
+    logfile <<cur_time<<","<<param.pos_x <<","<<param.pos_y <<","<<param.pos_z <<","                           //set_pos
+        <<pos_drone.pose.position.x <<","<<pos_drone.pose.position.y <<","<<pos_drone.pose.position.z <<","    //uav_pos
+        <<vel_target.x <<","<<vel_target.y <<","<<vel_target.z <<","                                           //set_vel
+        <<vel_drone.twist.linear.x <<","<<vel_drone.twist.linear.y <<","<<vel_drone.twist.linear.z <<","       //uav_vel
+        <<angle_target.x  <<","<<angle_target.y  <<","<<angle_target.z  <<","                                  //set_att
+        <<angle_receive.x <<","<<angle_receive.y <<","<<angle_receive.z <<","                                  //uav_att
+        <<thrust_target<<std::endl;
+
 }
