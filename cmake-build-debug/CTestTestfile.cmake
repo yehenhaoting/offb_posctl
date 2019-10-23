d51a7457442eff42d3827d18674fafa8f3ee31af
zm <yehenhaoting@sjtu.edu.cn>
2019-10-23 15:52:29 +0800

    basic PID control for study

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zm/catkin_ws/src/offb_posctl
-# Build directory: /home/zm/catkin_ws/src/offb_posctl/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
