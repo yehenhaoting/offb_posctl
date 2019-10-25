e7439ef150124e9ca0e99e2032127bb1a232afd4
孙东晓 <sundxfansky@sjtu.edu.cn>
2019-10-25 09:53:37 +0800

    before merge

---
+++ cmake-build-debug/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/catkin_px4_OFFBOARD/src/offb_posctl/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/ubuntu/catkin_px4_OFFBOARD/src/offb_posctl/cmake-build-debug/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/ubuntu/catkin_px4_OFFBOARD/src/offb_posctl/cmake-build-debug/devel/lib:/home/ubuntu/catkin_px4_OFFBOARD/devel/lib:/home/ubuntu/catkin_mz/devel/lib:/home/ubuntu/iswarm/ros_ws/devel/lib:/home/ubuntu/Rotors/devel/lib:/home/ubuntu/catkin_ws/devel/lib:/opt/ros/indigo/lib:/usr/local/lib:/usr/local/cuda-6.5/lib64:/usr/local/Trolltech/Qt-4.8.7//lib:/usr/local/cuda/lib64"
+export PATH="/home/ubuntu/Rotors/devel/bin:/opt/ros/indigo/bin:/usr/local/cuda-6.5/bin:/usr/local/Trolltech/Qt-4.8.7//bin:/usr/local/cuda/bin:/home/ubuntu/gcc-arm-none-eabi-5_4-2016q2/bin:/home/ubuntu/gcc-arm-none-eabi-4_9-2015q3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+export PYTHONPATH="/home/ubuntu/catkin_px4_OFFBOARD/src/offb_posctl/cmake-build-debug/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/catkin_px4_OFFBOARD/src/offb_posctl/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/ubuntu/catkin_px4_OFFBOARD/src/offb_posctl:$ROS_PACKAGE_PATH"
